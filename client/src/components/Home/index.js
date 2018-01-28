4df4b516bd89abfedf373c7b2fb262c36d328108
ameliejyc <amelie.chan@gmail.com>
2018-01-28 19:00:08 +0000

    :feet: Started styling the home page for funsies

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, TextContainer } from './index.styles';
+
+const Home = () => (
+  <Container>
+    <TextContainer>
+      <h1>Built for developers</h1>
+      <p>
+        DickHub is a platform inspired by the way you have to work. From <strong>dicks</strong> to{' '}
+        <strong>assholes</strong>, you can share experiences, vent, and find solidarity alongside
+        millions of other developers.
+      </p>
+    </TextContainer>
+  </Container>
+);
+
+export default Home;
